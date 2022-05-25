b5e6356c30823949279c21bad84acaa0817b43db
Srinath Venkatesh <srinathv@udel.edu>
2022-05-25 15:33:50 -0400

    recordings list radio select working

--- src/components/RecordingsList.tsx
+++ src/components/RecordingsList.tsx
@@ -1,6 +1,7 @@
 // Source Imports
 import React, { useState } from "react";
 import { FlatList, View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import ExpandedRecording from "./ExpandedRecording";
 
 export default function RecordingsList(): JSX.Element {
     const [data, setData] = useState<{ name: string, expand: boolean }[]>([
@@ -9,29 +10,33 @@ export default function RecordingsList(): JSX.Element {
         { name: "Recording 3", expand: false },
     ]);
 
-
     const renderItem = ({ item, index }: {
         item: { name: string, expand: boolean }, index: number
-    }) => (
-        <TouchableOpacity onPress={() => expandRecording(item)}>
-            <View key={index} style={styles.item}>
-                <Text style={styles.title}>{item.name}</Text>
-                <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "baseline" }}>
-                    <Text>May 17, 2019</Text>
-                    <Text>3:02</Text>
+    }) => {
+        return (
+            <TouchableOpacity onPress={() => expandRecording(item)}>
+                <View key={index} style={styles.item}>
+                    <Text style={styles.title}>{item.name}</Text>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "baseline" }}>
+                        <Text>May 17, 2019</Text>
+                        <Text>3:02</Text>
+                    </View>
+                    {item.expand && <ExpandedRecording />}
                 </View>
-            </View>
-        </TouchableOpacity>
-    );
+            </TouchableOpacity>
+        );
+    };
 
     function expandRecording(item: { name: string, expand: boolean }) {
-        data.forEach(recording => {
+        const dataCopy = [ ...data ];
+
+        dataCopy.forEach(recording => {
             recording.expand = false;
             if (recording === item) {
                 recording.expand = true;
             }
         });
-        setData(data);
+        setData(dataCopy);
         console.log(data);
     }
 
